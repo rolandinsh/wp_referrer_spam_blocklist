8c1a71cca0b37fec43e2dbe055dd34d9deb5f9c6
rolandinsh <rolands@mediabox.lv>
2015-07-10 19:05:26 +0200

    better description for Amazon DE wishlist link

--- wpReferralBlackList.php
+++ wpReferralBlackList.php
@@ -108,7 +108,7 @@ class wpReferralBlacklist
  
         if ($file == WPRSBFILE) {
             return array_merge($links, array(
-                '<a href="http://www.amazon.de/registry/wishlist/3ARHPQ1SLAMPV?tag=rolandinshde-21">' . __("My Amazon wishlist", "wprsb") . '</a>',
+                '<a href="http://www.amazon.de/registry/wishlist/3ARHPQ1SLAMPV?tag=rolandinshde-21">' . __("My Amazon.DE wishlist", "wprsb") . '</a>',
                 '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=Z4ALL9WUMY3CL&lc=LV&item_name=Umbrovskis.%20WordPress%20plugins&item_number=004&currency_code=EUR&bn=PP-DonationsBF:btn_donate_SM.gif:NonHosted">' . __('Donate via PayPal', 'wprsb') . '</a>'
             ));
         }
