2d594ce8660c0399b8cc6eabc27667b00700467f
rolandinsh <rolands@mediabox.lv>
2015-08-09 05:43:07 +0200

    version match

--- wp_referral_black_list.php
+++ wp_referral_black_list.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: WP referrer spam blacklist
- * Plugin URI: http://http://simplemediacode.com/?utm_source=WPplugin%3Awp-referrer-spam-blacklist&utm_medium=wordpressplugin&utm_campaign=FreeWordPressPlugins&utm_content=v-1-0-4
+ * Plugin URI: http://http://simplemediacode.com/?utm_source=WPplugin%3Awp-referrer-spam-blacklist&utm_medium=wordpressplugin&utm_campaign=FreeWordPressPlugins&utm_content=v-1-0-5
  * Description: WordPress plugin to fight with referrer spammers (like semalt, buttons-for-website and many more) and do not mess Google Analytics
  * Version: 1.0.5
  * Stable tag: 1.0.5
  * Requires at least: 4.0
- * Tested up to: 4.2.3
+ * Tested up to: 4.2.4
  * Author: Rolands Umbrovskis
  * Author URI: http://umbrovskis.com
  * License: SimpleMediaCode
