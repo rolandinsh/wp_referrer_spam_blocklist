93522e572f968ee370b40d9f084ba547c7985595
rolandinsh <rolands@mediabox.lv>
2016-08-04 00:02:31 +0300

    list update

--- wp_referral_black_list.php
+++ wp_referral_black_list.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: WP referrer spam blacklist (Ghost Spam in Google Analytics)
- * Plugin URI: https://simplemediacode.com/?utm_source=WPplugin%3Awp-referrer-spam-blacklist&utm_medium=wordpressplugin&utm_campaign=FreeWordPressPlugins&utm_content=v-1.2.201605231
+ * Plugin URI: https://simplemediacode.com/?utm_source=WPplugin%3Awp-referrer-spam-blacklist&utm_medium=wordpressplugin&utm_campaign=FreeWordPressPlugins&utm_content=v-1.2.201608031
  * Description: WordPress plugin to fight with referrer spammers (like semalt, buttons-for-website and many more) and do not mess Google Analytics
- * Version: 1.2.201607041
- * Stable tag: 1.2.201607041
+ * Version: 1.2.201608031
+ * Stable tag: 1.2.201608031
  * Requires at least: 4.0
  * Tested up to: 4.5.3
  * Author: Rolands Umbrovskis
